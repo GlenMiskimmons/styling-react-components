34aac48754e908b25926f903baf76a656548177c
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-02-28 13:33:06 +0000

    Module 04 Comparing Styling with Examples
    CSS Stylesheets

--- 03-stylesheet/before/src/App.js
+++ 03-stylesheet/before/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import "./App.css";
+import "./NewsletterOverride.css";
 
-import './App.css'
-import Newsletter from './Newsletter.js'
+import React from "react";
+
+import Newsletter from "./Newsletter.js";
 
 function App() {
   return (
     <div className="app__newsletter">
       <Newsletter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
